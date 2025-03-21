c8f1d3089637e6a9f1d9200904f4bd66dc079c56
Vilizar Denichin <denichinv@gmail.com>
2025-03-21 15:39:21 +0000

    structure and design

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
 
-import './App.css'
 
 function App() {
   return (
-    <>
+    <div  className='text-9xl  outline-solid flex justify-center items-center h-dvh bg-[url(assets/wallpaper.jpg)] bg-no-repeat bg-cover'>
 
-<div className='text-9xl'>
+<div className="font-mono font-bold  backdrop-blur-xs backdrop-opacity-80 backdrop-brightness-92 w-full  py-10 text-center">
   00:00:00
 </div>
-    </>
+    </div>
   )
 }
 
