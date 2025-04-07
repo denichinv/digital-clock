b9563fa0f99583351f4c00cd422334b0f3b3d7f7
Vilizar Denichin <denichinv@gmail.com>
2025-04-07 17:03:09 +0100

    page made responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,23 @@
 import { useEffect, useState } from "react"
 
-
 function App() {
-  
-  
-
-
-  const [time,setTime] = useState(new Date());
+  const [time, setTime] = useState(new Date());
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTime(new Date())
-    },1000);
+    }, 1000);
 
     return () => clearInterval(interval)
   }, [])
 
-
-
-  
-
   return (
-    <div  className='  outline-solid flex justify-center items-center h-dvh bg-[url(assets/wallpaper.jpg)] bg-no-repeat bg-cover'>
+    <div className="flex justify-center items-center h-dvh bg-[url(assets/wallpaper.jpg)] bg-no-repeat bg-cover bg-center">
 
-<div className="font-mono font-bold  backdrop-blur-xs backdrop-opacity-80 backdrop-brightness-92 w-full  py-5 text-center">
-      <h1 className=" text-9xl py-2px">{time.toLocaleTimeString("en-GB", { hour12: false })}</h1>
-      <h2 className="text-7xl py-2">{time.toLocaleDateString("en-GB", { weekday: "long", day: "2-digit", month: "long", year: "numeric" })}</h2>
-</div>
+      <div className="font-mono font-bold backdrop-blur-sm backdrop-opacity-80 backdrop-brightness-90 w-full md:w-full lg:w-full py-5 text-center px-4">
+        <h1 className="text-5xl sm:text-7xl md:text-8xl lg:text-9xl py-2">{time.toLocaleTimeString("en-GB", { hour12: false })}</h1>
+        <h2 className="text-xl sm:text-3xl md:text-5xl lg:text-7xl py-2">{time.toLocaleDateString("en-GB", { weekday: "long", day: "2-digit", month: "long", year: "numeric" })}</h2>
+      </div>
 
     </div>
   )
